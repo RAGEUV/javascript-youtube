11f3d38fdd23653bcb108d89e506700d59c1cbf8
Ujjwal Verma <85752903+RAGEUV@users.noreply.github.com>
2024-04-24 13:23:28 +0000

    discusssion on variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 421376
+let accountEmail = "ujjwal@gamil.com"
+var accountPassword = "34251"
+accountCity = "Bhilai" 
+
+
+//console.log(accountId)
+/*
+NOTE:- Never use var for declaring a variable as it causes ambiguity because javascript cannot deal with scopes.
+So instead of var always use 'let'..........
+*/
+accountEmail = "uv@gmail.com"
+accountPassword = "746746"
+accountCity = "Bengaluru"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
